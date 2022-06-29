2ea4827dc25f44c1423f23f43b44c146d4f66deb
herdal06 <h.erdal721@gmail.com>
2022-06-30 00:06:43 +0300

    Create all the fragments and add them to the navigation graph

---
+++ app/src/main/java/com/example/countriesapp/ui/fragments/onboarding/screens/FirstScreen.kt
@@ -0,0 +1,32 @@
+package com.example.countriesapp.ui.fragments.onboarding.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.countriesapp.R
+
+
+class FirstScreen : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
